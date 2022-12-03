80c742d5c873bbb660317c00d15b2f607d102c76
evgenijkorob <evgenij.k.wie@gmail.com>
2022-12-03 17:16:06 +0300

    feat(*): make test PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { LetModule, PushModule } from '@ngrx/component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,6 +15,12 @@ import { LetModule, PushModule } from '@ngrx/component';
     StoreModule.forRoot({}, {}),
     LetModule,
     PushModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
